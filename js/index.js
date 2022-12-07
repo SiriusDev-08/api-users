e4e3e2338b9f867de226330f40fbf894b9bbc96b
Márcio Bruno Loiola Gomes <marciogomes@alu.ufc.br>
2022-12-06 23:35:47 -0300

    feat: removing canvas and new sintax

--- js/index.js
+++ js/index.js
@@ -6,40 +6,7 @@ function startGame() {
   yellowGamePiece = new component(75, 75, "yellow", 250, 60);
   blueGamePiece = new component(75, 75, "blue", 450, 110);
   playerPiece = new spriteComponent(75, 75, "green", 50, 10);
-  myGameArea.start();
-  /* mySpritePlayer.start(); */
-  myGameTitle.start();
 }
-
-const myGameArea = {
-  canvas: document.createElement("canvas"),
-  start: function () {
-    this.canvas.width = 600;
-    this.canvas.height = 500;
-    this.canvas.x = 10000;
-    this.canvas.y = 100;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-  },
-  clear: function () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
-};
-
-const myGameTitle = {
-  title: document.createElement("h1"),
-  start: function () {
-    this.title.innerHTML = "River Raid";
-    this.title.height = 100;
-    this.title.width = 800;
-    document.body.insertBefore(this.title, document.body.childNodes[0]);
-    this.title.childNodes[0].style.textAlign = "center";
-    this.title.childNodes[0].style.fontSize = "50px";
-    this.title.childNodes[0].style.fontFamily = "Arial";
-    this.title.childNodes[0].style.color = "white";
-  },
-};
 
 const mySpritePlayer = {
   img: document.createElement("img"),
