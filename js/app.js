07c6c56b53e4c0cf3342885862e269b6a927d638
Márcio Bruno Loiola Gomes <marciogomes@alu.ufc.br>
2022-12-07 23:22:58 -0300

    feat: player img on screen

--- js/app.js
+++ js/app.js
@@ -1,22 +1,17 @@
 const game = document.querySelector(".game");
 
-function startGame() {
-  redGamePiece = new component(75, 75, "red", 50, 10);
-  yellowGamePiece = new component(75, 75, "yellow", 250, 60);
-  blueGamePiece = new component(75, 75, "blue", 450, 110);
-  playerPiece = new spriteComponent(75, 75, "green", 50, 10);
-}
-
-const mySpritePlayer = {
-  img: document.createElement("img"),
-  start: function () {
-    this.img.src = "_assets/playerPiece.png";
-    this.img.height = 75;
-    this.img.width = 75;
-    document.body.insertBefore(this.img, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-  },
-  clear: function () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
+const createPlayer = () => {
+  const player = document.createElement("img");
+  player.src = "_assets/playerPiece.png";
+  player.height = 75;
+  player.width = 75;
+  player.style.left = "50%";
+  player.style.top = "50%";
+  player.style.position = "absolute";
+  player.style.transform = "translate(-50%, -50%)";
+  game.appendChild(player);
 };
+
+function startGame() {
+  createPlayer();
+}
