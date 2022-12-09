c3ea22a631a5f3a9b6435383a501c35ebea8218f
Márcio Bruno Loiola Gomes <marciogomes@alu.ufc.br>
2022-12-09 13:40:04 -0300

    feat: game over div

--- js/app.js
+++ js/app.js
@@ -68,7 +68,34 @@ const checkPlayerLife = () => {
       playerLife = false;
       enemy.remove();
       player.remove();
-      alert("Game Over");
+      //div with game over text
+      const gameOver = document.createElement("div");
+      gameOver.id = "gameOver";
+      gameOver.style.height = "100vh";
+      gameOver.style.width = "100vw";
+      gameOver.style.position = "absolute";
+      gameOver.style.backgroundColor = "black";
+      gameOver.style.display = "flex";
+      gameOver.style.justifyContent = "center";
+      gameOver.style.alignItems = "center";
+      gameOver.style.color = "white";
+      gameOver.style.fontSize = "50px";
+      gameOver.innerHTML = "Game Over";
+      game.appendChild(gameOver);
+      //button to restart the game
+      const restart = document.createElement("button");
+      restart.id = "restart";
+      restart.style.height = "50px";
+      restart.style.width = "100px";
+      restart.style.backgroundColor = "black";
+      restart.style.color = "white";
+      restart.style.border = "none";
+      restart.style.fontSize = "20px";
+      restart.innerHTML = "Restart";
+      gameOver.appendChild(restart);
+      restart.addEventListener("click", () => {
+        location.reload();
+      });
     }
   }, 1000);
 };
@@ -85,10 +112,9 @@ const updateBackground = () => {
   }, 1000);
 };
 
-// destroying elements when they are out of the game area
+// destroy element when it reaches the bottom of the game div
 const destroyElement = () => {
   const enemy = document.querySelector("#enemy1");
-  const player = document.querySelector("#player");
   if (parseInt(enemy.style.top) > 600) {
     enemy.remove();
   }
